cb816aa7f7c2cc2af922f61efa112587126bd832
Senthil Kumaran <ursenix@gmail.com>
2016-08-06 16:38:06 +0530

    Test

--- app/employee/emp.service.ts
+++ app/employee/emp.service.ts
@@ -8,7 +8,7 @@ export class EmployeeService{
         let employees = [
             new Employee(1, 'Senthil', 20000),
             new Employee(2, 'Kumaran', 25000),
-            new Employee(3, 'Shakti', 10000),
+            new Employee(3, 'Shakti Senthil', 10000),
         ];
         return employees;
         
